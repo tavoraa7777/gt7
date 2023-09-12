ce54ac5a593047b2d0e773d43fa732f69ea82ad8
tavoraa7777 <144848998+tavoraa7777@users.noreply.github.com>
2023-09-12 18:18:21 -0300

    exc15

--- trab.sql
+++ trab.sql
@@ -36,3 +36,7 @@ RIGHT JOIN alunos ON cursos.id = alunos.curso_id;
 SELECT autores.nome, livros.titulo 
 FROM autores 
 LEFT JOIN livros ON autores.id = livros.autor_id;
+--exc15
+SELECT alunos.nome, cursos.nome AS curso 
+FROM alunos 
+INNER JOIN cursos ON alunos.curso_id = cursos.id;
